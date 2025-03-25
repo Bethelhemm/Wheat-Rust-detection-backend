ceae98a965fdccaf4f3f8325395a1739846496b0
Bethelhemm <gebeyehubetty92@gmail.com>
2025-03-25 17:52:58 +0300

    added root url

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("", RegisterView.as_view(), name="signup"),
     path("signup/", RegisterView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
