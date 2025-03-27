7497df222c66430ba9ae746d5408cee9dde722d5
Bethelhemm <gebeyehubetty92@gmail.com>
2025-03-27 20:40:00 +0300

    added the search functionality

--- community/serializers.py
+++ community/serializers.py
@@ -1,30 +1,37 @@
 from rest_framework import serializers
-from .models import Post, Comment, Like, SavedPost
-
+from .models import *
+from authentication.models import User
 
 class PostSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
+    user_name = serializers.CharField(source="user.name", read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ["id", "user", "text", "image", "audio", "created_at"]
+        fields = ["id", "user", "user_name", "text", "image", "audio", "created_at", "likes_count", "comments_count"]
 
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
+    user_name = serializers.CharField(source="user.name", read_only=True)
 
     class Meta:
         model = Comment
-        fields = ["id", "user", "post", "text", "created_at"]
+        fields = ["id", "user", "user_name", "post", "text", "created_at"]
 
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
-        fields = ["user", "post", "created_at"]
+        fields = ["id", "user", "post", "created_at"]
 
 
 class SavedPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = SavedPost
-        fields = ["user", "post", "created_at"]
+        fields = ["id", "user", "post", "created_at"]
