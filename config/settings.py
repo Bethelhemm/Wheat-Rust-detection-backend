d0cb67303411fe8d20e8e043c1905e3280e0400e
Bethelhemm <gebeyehubetty92@gmail.com>
2025-04-14 21:24:27 +0300

    fixed the media file upload

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
