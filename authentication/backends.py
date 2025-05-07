97a00549e8f7212866357dba672f4cb0685cfa91
Bethelhemm <gebeyehubetty92@gmail.com>
2025-05-07 18:31:10 +0300

    fixed the loginn issue

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,8 +1,12 @@
+import logging
 from django.contrib.auth.backends import ModelBackend
 from authentication.models import User
 
+logger = logging.getLogger(__name__)
+
 class EmailOrPhoneBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        logger.info(f"Authenticating user with username: {username}")
         if username is None:
             return None 
         try:
@@ -11,8 +15,11 @@ class EmailOrPhoneBackend(ModelBackend):
             else:
                 user = User.objects.get(phone=username)
         except User.DoesNotExist:
+            logger.warning(f"User not found for username: {username}")
             return None
 
         if user.check_password(password) and self.user_can_authenticate(user):
+            logger.info(f"User authenticated successfully: {username}")
             return user
+        logger.warning(f"Authentication failed for user: {username}")
         return None
