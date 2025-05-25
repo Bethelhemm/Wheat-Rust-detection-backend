964385bdcfdf4eb9336e7c46f7dcbf91c87bd912
Bethelhemm <gebeyehubetty92@gmail.com>
2025-05-25 08:22:32 +0300

    added the EmailOrPhoneBackend class to authentication/backends.py

--- authentication/backends.py
+++ authentication/backends.py
@@ -4,6 +4,27 @@ from django.core.exceptions import PermissionDenied
 
 User = get_user_model()
 
+class EmailOrPhoneBackend(ModelBackend):
+    """
+    Custom authentication backend to allow users to authenticate using either their email or phone number.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get(User.USERNAME_FIELD)
+        try:
+            # Try to fetch the user by email
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            try:
+                # Try to fetch the user by phone number
+                user = User.objects.get(phone_number=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
+
 class BannedUserBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
